724c59064d75c3e46ce1345532eb9c50b0bf6a6b
TarasYA <32075208+TarasYA@users.noreply.github.com>
2019-01-06 18:20:37 +0200

    Update DiscordBotReal.py

--- DiscordBotReal.py
+++ DiscordBotReal.py
@@ -1,101 +1,128 @@
 """
 Personal by me.
 """
-import discord
 import os
 from discord.ext import commands
 
-token = os.getenv("distok")
-client = commands.Bot(command_prefix="m!")
 
-file = open("DiscordServerNames.txt", "r+")
+TOKEN = os.getenv("distok")
+CLIENT = commands.Bot(command_prefix="m!")
 
 
-@client.event
+@CLIENT.event
 async def on_ready():
+    """
+    bot started working
+    """
     print("running")
 
 
-@client.event
+@CLIENT.event
 async def on_member_join(member):
+    """
+    when new member join
+    """
     file = open("DiscordServerNames.txt", "r+")
-    messages = []
     for channel in member.server.channels:
         if channel.name == "test":
-            await client.send_message(channel, "Привет, " + str(member.name) + "!")
-            await client.send_message(channel, "Зарегистрируйтесь, используя команду m!reg nickname(только слитно) realname.")
-            await client.send_message(channel, "Если, на нашем сервере у Вас больше чем 1 аккаунт, напишите никнеймы через запятую без пробела.")
-            await client.send_message(channel, "Используйте команду m!commands для того что бы узнать все команды бота.")
+            string = f"""
+Привет, + {str({member.name})}!
+Зарегистрируйтесь, используя команду m!reg nickname(только слитно) realname.
+Если, на нашем сервере у Вас больше чем 1 аккаунт, напишите никнеймы через запятую без пробела.
+Используйте команду m!commands для того что бы узнать все команды бота.
+"""
+            await CLIENT.send_message(channel, string)
         if channel.name == 'names':
             amount = 50
             messages = []
-            async for message in client.logs_from(channel, limit=int(amount) + 1):
+            async for message in CLIENT.logs_from(channel, limit=int(amount) + 1):
                 messages.append(message)
-            await client.delete_messages(messages)
-            await client.send_message(channel, "|=====================================================================|")
+            await CLIENT.delete_messages(messages)
+            await CLIENT.send_message(channel, "|==================================|")
             for line in file:
-                await client.send_message(channel, line)
-            await client.send_message(channel, "|=====================================================================|")
+                await CLIENT.send_message(channel, line)
+            await CLIENT.send_message(channel, "|==================================|")
     file.close()
 
 
-@client.command(pass_context=True)
+@CLIENT.command(pass_context=True)
 async def info(ctx):
+    """
+    info about server
+    """
     channel = ctx.message.channel
-    await client.send_message(channel, """Сервер для фолс программистов от тру быдлокодеров...""")
+    await CLIENT.send_message(channel, """Сервер для фолс программистов от тру быдлокодеров...""")
 
-@client.command(pass_context=True)
+
+@CLIENT.command(pass_context=True)
 async def lol(ctx):
+    """
+    test
+    """
     channel = ctx.message.channel
-    await client.send_message(channel, "kek")
+    await CLIENT.send_message(channel, "kek")
 
-@client.command(pass_context=True)
-async def commands(ctx):
+
+@CLIENT.command(pass_context=True)
+async def command(ctx):
+    """
+    list of commands
+    """
     channel = ctx.message.channel
-    await client.send_message(channel, """
+    await CLIENT.send_message(channel, """
 Список команд:
 m!reg nickname realname - регистрация на сервере;
 m!clear count - стирание текста на сервере;
 m!info - информация о сервере;
 m!names - имена людей на сервере;
-m!commands - команды на сервере.
+m!command - команды на сервере.
 """)
 
 
-@client.command(pass_context=True)
+@CLIENT.command(pass_context=True)
 async def reg(ctx, nickname, realname):
+    """
+    register on server
+    """
     channel = ctx.message.channel
     nickname = str(nickname)
     realname = str(realname)
     msg = "\n" + nickname + "-" + realname
-    if(len(msg) > 20):
-        await client.send_message(channel, "Слишком большие входные данные!")
+    if len(msg) > 20:
+        await CLIENT.send_message(channel, "Слишком большие входные данные!")
     else:
         with open("DiscordServerNames.txt", 'a') as file:
             file.writelines(msg)
-        await client.send_message(channel, "Спасибо за регистрацию!")
+        await CLIENT.send_message(channel, "Спасибо за регистрацию!")
         file.close()
 
 
-@client.command(pass_context=True)
+@CLIENT.command(pass_context=True)
 async def names(ctx):
+    """
+    list of names in server
+    """
     file = open("DiscordServerNames.txt", "r+")
     channel = ctx.message.channel
     for line in file:
-        await client.send_message(channel, line)
+        await CLIENT.send_message(channel, line)
     file.close()
 
 
-@client.command(pass_context=True)
+@CLIENT.command(pass_context=True)
 async def clear(ctx, amount=100):
+    """
+    chat clearing
+    """
     channel = ctx.message.channel
-    if(amount >= 2):
-        messages = []
-        async for message in client.logs_from(channel, limit=int(amount) + 1):
-            messages.append(message)
-        await client.delete_messages(messages)
-    else:
-        await client.send_message(channel, "Введите значение которое равно или больше 2!")
 
-client.run(token)
+    if amount >= 2:
+        messages = []
+        async for message in CLIENT.logs_from(channel, limit=int(amount) + 1):
+            messages.append(message)
+        await CLIENT.delete_messages(messages)
+    else:
+        await CLIENT.send_message(channel, "Введите значение которое равно или больше 2!")
+
+CLIENT.run(TOKEN)
 print('stopped')
