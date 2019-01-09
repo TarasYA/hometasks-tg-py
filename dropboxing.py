0e98385cbab27c80eab8cf0757d5fbd412bd0508
TarasYA <32075208+TarasYA@users.noreply.github.com>
2019-01-09 18:12:18 +0200

    Update dropboxing.py

--- dropboxing.py
+++ dropboxing.py
@@ -1,29 +1,40 @@
 import os
 import dropbox
 
-dbx = dropbox.Dropbox(os.getenv("dropbox"))
+DBX = dropbox.Dropbox(os.getenv("dropbox"))
 
 
 def downloading_file(file_from):
-    with open(file_from, "wb") as f:
-        metadata, res = dbx.files_download(path="/"+file_from)
-        f.write(res.content)
+    """
+    downloading file
+    """
+    with open(file_from, "wb") as file:
+        metadata, res = DBX.files_download(path="/"+file_from)
+        file.write(res.content)
 
 
 def upload_file(file_from):
+    """
+    uploading new file
+    """
     deleting_file(file_from)
-    f = open(file_from, 'rb')
-    dbx.files_upload(f.read(), "/"+file_from)
+    file = open(file_from, 'rb')
+    DBX.files_upload(file.read(), "/"+file_from)
 
 
 def deleting_file(file_from):
-    dbx.files_delete("/"+file_from)
+    """
+    delete file
+    """
+    DBX.files_delete("/"+file_from)
 
 
 def checking_exist(file_from):
+    """
+    checking if file exist
+    """
     try:
-        dbx.files_get_metadata("/"+file_from)
+        DBX.files_get_metadata("/"+file_from)
         return True
     except Exception:
         return False
-
