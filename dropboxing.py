9815a1eb60379fd21c0957111957aad796a86bdf
TarasYA <32075208+TarasYA@users.noreply.github.com>
2019-01-09 16:04:22 +0200

    Add files via upload

---
+++ dropboxing.py
@@ -0,0 +1,28 @@
+import os
+import dropbox
+
+dbx = dropbox.Dropbox(os.getenv("dropbox"))
+
+
+def downloading_file(file_from):
+    with open(file_from, "wb") as f:
+        metadata, res = dbx.files_download(path="/"+file_from)
+        f.write(res.content)
+
+
+def upload_file(file_from):
+    deleting_file(file_from)
+    f = open(file_from, 'rb')
+    dbx.files_upload(f.read(), "/"+file_from)
+
+
+def deleting_file(file_from):
+    dbx.files_delete("/"+file_from)
+
+
+def checking_exist(file_from):
+    response = dbx.files_list_folder(path="")
+
+    if file_from in response:
+        return True
+    return False
